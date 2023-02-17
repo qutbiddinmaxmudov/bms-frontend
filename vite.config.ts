3b972b8763d8996c79f0e03616fbb3e6c77b3d51
Maxmudov <qutbiddinmaxmudov@mail.ru>
2023-02-17 21:42:51 +0500

    eslint base config

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,7 @@ import eslint from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint({
-    
-  })],
+  plugins: [react(), eslint()],
   server: {
     port: 3000,
   },
